e96da8993b81b71ba6024dfc8f956b77174e0b67
Wesley Obenshain <umdshaman@gmail.com>
2018-03-29 01:36:20 -0400

    added other data

--- index.js
+++ index.js
@@ -23,43 +23,16 @@ app.post('/GSE', function(req, res, next) {
 
 app.post('/runXYZ', function(req, res, next) {
   console.log('/runXYZ');
-  // short-circuit
-  res.send({"showme": 1});
-  return next();
-  // "Real" code
   R("runXYZ.R")
-    .data(JSON.stringify(req.body))
+//    .data(JSON.stringify(req.body))
     .call((err,returnValue) => {
-      returnValue = JSON.parse(decodeURI(returnValue));
-      if (returnValue.error === undefined) {
-        res.send(returnValue['saveValue']);
-      } else {
-        return next(returnValue.error.statusMessage);
-      }
-    });
-});
-
-app.get('/getRawhist', function(req, res, next) {
-  console.log('/getRawhist');
-  R("getRawhist.R")
-    .call((err, returnValue) => {
-      if (err) return next(err);
       fs.readFile(returnValue,(err,data) => {
-        if (err) return next(err);
-        res.send(data);
-        fs.unlinkSync(returnValue);
-      });
-    });
-});
-
-app.get('/getRmahist', function(req, res, next) {
-  console.log('/getRmahist');
-  R("getRmahist.R")
-    .call((err, returnValue) => {
-      if (err) return next(err);
-      fs.readFile(returnValue,(err,data) => {
-        if (err) return next(err);
-        res.send(data);
+        data = JSON.parse(decodeURI(data));
+        if (data.error === undefined) {
+          res.send(data['saveValue']);
+        } else {
+          return next(data.error.statusMessage);
+        }
         fs.unlinkSync(returnValue);
       });
     });
--- web/javascript/startup.js
+++ web/javascript/startup.js
@@ -1,6 +1,4 @@
 var app = {
-    models: {},
-    views: {},
     events: {
         updateModel: function (e) {
             var e = $(e.target);
@@ -10,5 +8,27 @@ var app = {
                 this.model.set(e.attr('name') || e.attr('id'), !e.hasClass('selectized') ? e.val() : e.val().length > 0 ? e.val().split(',') : []);
             }
         }
-    }
+    },
+    functions: {
+        histRtoJS: function(data) {
+            var reorder = Array(data.x[0].length).fill({x:[], y:[]});
+            for (var i = 0; i < reorder.length; i++) {
+                reorder[i] = {
+                    x:[],
+                    y:[],
+                    mode: 'markers',
+                    type: 'scatter'
+                };
+            }
+            for (var i = 0; i < data.x.length; i++) {
+                for (var j = 0; j < data.x[i].length; j++) {
+                    reorder[j].x.push(data.x[i][j]);
+                    reorder[j].y.push(data.y[i][j]);
+                }
+            }
+            return reorder;
+        }
+    },
+    models: {},
+    views: {}
 };
