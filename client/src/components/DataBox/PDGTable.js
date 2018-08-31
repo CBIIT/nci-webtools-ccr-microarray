4d1f8c3ff6e5257336e31ccab3878271c4b1c712
jonkiky <jonkiky@gmail.com>
2018-08-31 15:53:26 -0400

    Change sorting add option for all dataset sorting
    Former-commit-id: fcd3481b1725d469a5882b22528bb642086a3644 [formerly d860cf5a06f53066f2154da07bbf024dd3cfa8bc [formerly 43304e49db6439574289a3aa9b3ce34e8acbe1e2]]
    Former-commit-id: 6a06eb2a036175edbceaa36365b6435ee4a7f968
    Former-commit-id: b0486ebc8d45348ab9738dd9da4be670a6340bc4

--- client/src/components/DataBox/PDGTable.js
+++ client/src/components/DataBox/PDGTable.js
@@ -1,76 +1,263 @@
 import React, { Component } from 'react';
-import { Table ,Select} from 'antd';
+import { Table,Input,message,Modal,Button} from 'antd';
+const Search = Input.Search;
+
 
 class PUGTable extends Component {
 
+
 	constructor(props){
 		super(props);
+
+	    this.state = {
+    	term:"" ,
+    	heapMap:"",
+    	visible: false
+	 	}
 	}
 
-	componentDidMount(){
-	}
 
+ handleOk = () => {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  }
+
+  handleCancel = () => {
+    this.setState({ group:"",selected:[],visible: false });
+  }
+
+
+	showHeapMap(row, idx, event){
+		 // not reflected in interface 
+		 let reqBody = {};
+			 reqBody.projectId=this.props.data.projectID;
+			 reqBody.group1=this.props.data.group_1;
+			 reqBody.group2=this.props.data.group_2;
+			 reqBody.upOrDown="downregulated_pathways";
+			 reqBody.pathway_name=row.Description;
+
+		fetch('./api/analysis/pathwaysHeapMap',{
+			method: "POST",
+			body: JSON.stringify(reqBody),
+			credentials: "same-origin",
+			headers: {
+		        'Content-Type': 'application/json'
+		    }
+		}).then(
+				res => res.json()
+				)
+			.then(result => {
+
+				if(result.status==200){
+					if(Object.keys(result.data).length === 0 && result.data.constructor === Object){
+
+						 message.success('no rows to aggregate');
+						 	
+					}else{
+
+						var link = "./images/"+this.props.data.projectID+"/"+result.data
+						this.setState({
+					      heapMap:link,
+					      visible: true
+					    });
+					}
+					
+
+				}else{
+					 message.success('no rows to aggregate');	
+				}
+
+			})
+
+
+	}
 
 
 	
   render() {
   	
+  	const {visible} = this.state;
   	let content ="";
-  	if(this.props.data.pathways_down.length > 0){
+  	var content_length =this.props.data.pathways_up.length;
+  	if(this.props.data.pathways_up.length > 0){
 		const columns = [{
 		  title: 'Pathway_ID',
 		  dataIndex: 'Pathway_ID',
-		  sorter: (a, b) => a["Pathway_ID"]-b["Pathway_ID"],
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a["Pathway_ID"]-b["Pathway_ID"];
+		  	}
+		  }
+		  	
 		}, {
 		  title: 'Source',
 		  dataIndex: 'Source',
-		  sorter: (a, b) => ('' + a.source).localeCompare(b.source),
+		  sorter:function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return ('' + a.Source).localeCompare(b.Source);
+		  	}
+		  }
 		}, {
 		  title: 'Description',
 		  dataIndex: 'Description',
-		  sorter: (a, b) => ('' + a.description).localeCompare(b.description),
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return ('' + a.Description).localeCompare(b.Description);
+		  	}
+		  }
 		}, {
 		  title: 'Type',
 		  dataIndex: 'Type',
-		  sorter: (a, b) => ('' + a.type).localeCompare(b.type),
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return ('' + a.Type).localeCompare(b.Type);
+		  	}
+		  }
 		}, {
-		  title: 'P_Val',
-		  dataIndex: 'P_Val',
-		  sorter: (a, b) => a.pval-b.pval,
+		  title: 'P_Value',
+		  dataIndex: 'P_Value',
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a.P_Value-b.P_Value;
+		  	}
+		  }
 		}, {
 		  title: 'FDR',
 		  dataIndex: 'FDR',
-		  sorter: (a, b) => a.fdr-b.fdr,
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a.FDR-b.FDR;
+		  	}
+		  }
 		}, {
 		  title: 'Ratio',
 		  dataIndex: 'Ratio',
-		  sorter: (a, b) => a.ratio-b.ratio,
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a.Ratio-b.Ratio;
+		  	}
+		  }
 		}, {
 		  title: 'Gene_List',
 		  dataIndex: 'Gene_List',
-		  sorter: (a, b) => ('' + a["gene.list"]).localeCompare(b["gene.list"]),
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return ('' + a["Gene_List"]).localeCompare(b["Gene_List"]);
+		  	}
+		  }
 		}, {
 		  title: 'Number_Hits',
 		  dataIndex: 'Number_Hits',
-		  sorter: (a, b) => a.hits-b.hits,
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a.Number_Hits-b.Number_Hits;
+		  	}
+		  }
 		}, {
 		  title: 'Number_Genes_Pathway',
 		  dataIndex: 'Number_Genes_Pathway',
-		  sorter: (a, b) => a['nb.genes.path']-b['nb.genes.path'],
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a['Number_Genes_Pathway']-b['Number_Genes_Pathway'];
+		  	}
+		  }
 
 		},{
 		  title: 'Number_User_Genes',
 		  dataIndex: 'Number_User_Genes',
-		  sorter: (a, b) => a['nb.user.genes']-b['nb.user.genes'],
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a['Number_User_Genes']-b['Number_User_Genes'];
+		  	}
+		  }
 		},{
 		  title: 'Total_Number_Genes',
 		  dataIndex: 'Total_Number_Genes',
-		  sorter: (a, b) => a['tot.back.genes']-b['tot.back.genes'],
+		  sorter: function(a, b) {
+		  	if(content_length>15000){
+		  		console.log(content_length);
+		  		// to be finished
+		  	}else{
+		  		return a['Total_Number_Genes']-b['Total_Number_Genes'];
+		  	}
+		  }
 		}];
+		
 
-		const data = this.props.data.pathways_down;
+		 const data = this.props.data.pathways_down;
+		
 			
-  	  	content = <Table columns={columns} dataSource={data} />;
+  	  	  const searchFilter = (row) => {
+	    	if(this.state.term ===""){
+	    		return true;
+	    	}
+
+	    	if (row["Source"].includes(this.state.term)) return true;
+	    	if (row["Description"].includes(this.state.term)) return true;
+	    	if (row["Type"].includes(this.state.term)) return true;
+	    	if (row["Pathway_ID"].includes(this.state.term)) return true;
+
+	    	return false;
+	    }
+
+			
+
+  	  	 // define group modal
+	    let modal = <Modal visible={visible}  onOk={this.handleOk} onCancel={this.handleCancel}
+	        footer={[
+	            <Button key="back" onClick={this.handleCancel}>Close</Button>,
+	          ]}
+	        >
+	          <img src={this.state.heapMap} style={{width:"75%"}} alt="heapMap"/>
+	        </Modal>
+	    // end  group modal
+
+		content=<div>
+					<div><Search  placeholder="input search text" className="input-search-for-deg-path" onSearch={value => this.setState({term: value})} /></div>
+					<div><Table 
+							columns={columns} 
+							dataSource={data.filter(searchFilter,this) }   
+							onRowClick={(row, idx, event)=>this.showHeapMap(row, idx, event)}  
+					      /> 
+					      {modal}
+      	</div>
+				</div>	
+
   	}else{
 		content=<div>No data</div>				
   	}
