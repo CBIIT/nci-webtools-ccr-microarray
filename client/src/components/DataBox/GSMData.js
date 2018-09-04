3eb49c14e2a9d1120b0647a20e4c665d8222ace0
jonkiky <jonkiky@gmail.com>
2018-09-04 13:21:05 -0400

    Show the full label when mouse over the axis label
    Former-commit-id: b72027ed95e3124a5d005057a9c78edaba6d0435 [formerly 0661603e4aab84f63ef740340e8fea33d0141a6e [formerly cae0bc0e2c5f150bd5a37aa751baeb3a8f5364de]]
    Former-commit-id: 8ed7492b7230db7af708b56389984aaa45e53877
    Former-commit-id: 1074920c920a8cfc1528dddde8659c0ad7939d19

--- client/src/components/DataBox/GSMData.js
+++ client/src/components/DataBox/GSMData.js
@@ -9,6 +9,58 @@ const Option = Select.Option;
 class GSMData extends Component {
 
 
+
+	componentDidMount() {
+	    // When the component is mounted, add your DOM listener to the elem.
+	    // (The "nv" elem is assigned in the render function.)
+	   	document.getElementById("tag3").addEventListener("mouseover", function(e){
+    	if(e.target.localName === "use"){
+    		let words = e.target.parentElement.cloneNode(true);
+    		let defs =e.target.parentElement.parentElement.parentElement.childNodes[1].cloneNode(true)
+    		var last_o_y=0;
+    		var last_t_y=0;
+    		for(let i = words.children.length-1; i>=0; i--){
+    			// change x and y accordinate
+    			words.children[i].setAttribute("x",20);
+    			if(last_o_y===0){
+    				// the last element
+    				last_o_y=words.children[i].getAttribute("y");
+    				last_t_y=20;
+    				words.children[i].setAttribute("y",20);
+    			}else{
+    				let diff = words.children[i].getAttribute("y")-last_o_y;
+    				last_o_y=words.children[i].getAttribute("y");
+					words.children[i].setAttribute("y",last_t_y+diff);
+					last_t_y=last_t_y+diff
+    			}
+    		}
+
+    		if(document.getElementById('tag3-tooltip-defs').firstChild!==null){
+			document.getElementById('tag3-tooltip-defs').removeChild(document.getElementById('tag3-tooltip-defs').firstChild)
+			}
+			document.getElementById('tag3-tooltip-defs').appendChild(defs);
+			if(document.getElementById('tag3-tooltip-svg').firstChild!==null){
+	    		document.getElementById('tag3-tooltip-svg').removeChild(document.getElementById('tag3-tooltip-svg').firstChild)
+			}
+    		document.getElementById('tag3-tooltip-svg').appendChild(words);
+
+    		// change tootltip svg position
+    		document.getElementById('tag3-tooltip').style.display = "block";
+	    	document.getElementById('tag3-tooltip').style.left = e.clientX -365 + "px";
+	    	document.getElementById('tag3-tooltip').style.top = e.clientY -100+ "px";
+
+	    	}
+
+	    	
+		});
+
+		// document.getElementById("tag3-svg-content").addEventListener("mouseleave", function(e){
+  //   	document.getElementById('tag3-tooltip').style.display = "none";
+		// });
+
+  	}
+
+
  state = {
     selectedRowKeys: [], // Check here to configure the default column
     loading: false,
@@ -121,6 +173,48 @@ class GSMData extends Component {
 				</div>	
   	  	
   	}else{
+  			  let tooltip = {
+						"position": "absolute",
+					    "background": "white",
+					    "width":"100%"
+					    
+					}
+		let tooltip_svg_div={
+					   "transform": "rotate(90deg)",
+					   "position": "absolute",
+					   "top": "-365px",
+					   "left": "-125px"
+					}
+
+		let tooltip_svg_title={
+						"padding-top": "20px"
+					}
+
+	 content =<div id="tag3" className="plot2">
+	 			<div id="tag3-tooltip" style={tooltip}>
+							<div id="tag3-tooltip-svg-title" style={tooltip_svg_title}></div>
+							<div id="tag3-tooltip-svg-div">Point:
+							<svg style={tooltip_svg_div} 
+								 xmlns="http://www.w3.org/2000/svg"
+								 xmlnsXlink="http://www.w3.org/1999/xlink" 
+								 width="40pt" 
+								 height="600pt" 
+								 viewBox="0 0 40 600" 
+								 version="1.1" >
+								 <defs id="tag3-tooltip-defs">  </defs>
+								 <g id="tag3-tooltip-svg">  </g>
+							</svg>
+							</div>
+							</div>
+							<div id="tag3-svg-content">
+							<ReactSVG  
+	  						path="./images/test/boxplotsAfterNorm.svg"
+	  						renumerateIRIElements={false}
+						 	svgClassName="svg-class-name"
+						 	className="wrapper-class-name"/>
+						 	</div>
+
+					</div>;
 
   	}
 
