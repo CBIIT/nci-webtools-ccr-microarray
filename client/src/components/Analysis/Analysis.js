90e6a216c8b0a8512541af31b3e40d6908cb4918
jonkiky <jonkiky@gmail.com>
2018-10-09 09:23:20 -0400

    remove hard code assession Code
    Former-commit-id: ab9f74e1f794565af8a30d25906108792a4aae55 [formerly 3a8623b4d5044623cfb2f5ff3783b39e5cbe03fc [formerly 749b203e157f13af081f6180ce67b4d0f25c8242]]
    Former-commit-id: 63fe0e53d9b32588fe477fd95ecdf9b513a8e362
    Former-commit-id: 52101b3a441c8ae9e6a645c3a5c797cb36bab839

--- client/src/components/Analysis/Analysis.js
+++ client/src/components/Analysis/Analysis.js
@@ -12,7 +12,7 @@ class Analysis extends Component {
             workflow: {
                 projectID: "",
                 analysisType: "0",
-                accessionCode: "GSE37874",
+                accessionCode: "",
                 fileList: [],
                 uploading: false,
                 progressing: false,
