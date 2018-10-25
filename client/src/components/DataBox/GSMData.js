512ef83a534504e110d7245c03d6c39f63791123
jonkiky <jonkiky@gmail.com>
2018-10-25 13:17:45 -0400

    rebuild the app
    Former-commit-id: 3acc9bdf2be56f562e2a2df44000edba20e2f6ec [formerly 2fe8b90df43cef05546e320078d6fe6e34beea99 [formerly e7d1a17f5c32a57463ec0aedc818c729b1c200ee]]
    Former-commit-id: 82f5cbc677021153a43947a972438438d279259b
    Former-commit-id: ad265e032f436550127deb20691faae24c3a32ea

--- client/src/components/DataBox/GSMData.js
+++ client/src/components/DataBox/GSMData.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Table, Select, Input, Tooltip } from 'antd';
+import { CustomCheckbox } from './CustomCheckbox';
 import ReactSVG from 'react-svg'
 
 const Search = Input.Search;
 const Option = Select.Option;
+const Selections = []
 
 class GSMData extends Component {
 
@@ -37,10 +39,38 @@ class GSMData extends Component {
     handleSelectAll = () => {
         console.log("handle select all ")
     }
-    handleSelection=(key)=>{
+    handleSelection = (key) => {
         console.log(key)
+        let selectedRowKeys = this.getCheckedBoxes("select-GSM")
 
-        
+
+    }
+
+    // Pass the checkbox name to the function
+    getCheckedBoxes(chkboxName) {
+        var checkboxes = document.getElementsByClassName(chkboxName);
+        // loop over them all
+        for (var i = 0; i < checkboxes.length; i++) {
+            // And stick the checked ones onto an array...
+            let key =parseInt(checkboxes[i].parentElement.parentElement.parentElement.getAttribute("data-row-key"));
+            if (checkboxes[i].checked) {
+
+                if (!Selections.includes(key)) {
+                    Selections.push(key);
+                }
+            } else {
+                // if it is uncheck and Seletions has the element
+                if (Selections.includes(key)) {
+                    var index = Selections.indexOf(key);
+                    if (index > -1) {
+                        Selections.splice(index, 1); //remove the element
+                    }
+                }
+            }
+
+        }
+        // Return the array if it is non-empty, or null
+        console.log(Selections)
     }
     render() {
 
@@ -52,13 +82,6 @@ class GSMData extends Component {
 
         if (this.props.data.dataList.length > 0) {
             const columns = [{
-                title: <div><label for="select-all" class="sr-only">Select ALl Items</label><input id="select-all" onClick={()=>{this.handleSelectAll()} } type="checkbox"/></div>,
-                dataIndex: 'select',
-                width: '15px',
-                render: (text, record, index) => (
-                    <div><label for="select-GSM" class="sr-only">Select ALl Items</label><input id="select-GSM" onClick={()=>{this.handleSelection(index)} } type="checkbox"/></div>
-                ),
-            }, {
                 title: 'GSM',
                 dataIndex: 'gsm',
                 width: '18%',
@@ -114,7 +137,7 @@ class GSMData extends Component {
 
                 return false;
             }
-               const rowSelection = {
+            const rowSelection = {
                 selectedRowKeys,
                 onChange: this.onSelectChange,
             };
