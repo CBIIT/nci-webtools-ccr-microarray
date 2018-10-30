8cd6cdd7d069d6dae03bf28a3775c677b8daad09
jonkiky <jonkiky@gmail.com>
2018-10-30 12:59:00 -0400

    add content to about page
    Former-commit-id: 8af1f322db9b3d8988f1224bc591925173fbaf43 [formerly 632081930de88b77fc0665deb21147e9c75529c6 [formerly c1a5411052aa8c69315d0add0afe6dbfbc8bfc68]]
    Former-commit-id: 0f8c3469e2dbc778b24666285868969aedd4bc3c
    Former-commit-id: 5c62da3c66a391bcd161b5971f32f5926e25d55f

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -10,14 +10,49 @@ class About extends Component {
 
     render() {
         return (
-                <div className="container container-board">
+            <div className="container container-board">
                     <section >
                         <h2>Microarrays</h2>
                         <div>
                             <p>
-                                Microarrays are a collection of DNA probes that are usually bound in defined positions to a solid surface, such as a glass slide, to which sample DNA fragments can be hybridised. The probes are generally oligonucleotides that are ‘ink-jet printed’ onto slides (Agilent) or synthesised in situ (Affymetrix). Labelled single-stranded DNA or antisense RNA fragments from a sample of interest are hybridised to the DNA microarray under high stringency conditions. The amount of hybridisation detected for a specific probe is proportional to the number of nucleic acid fragments in the sample.
+                                MicroArray Analysis Pipeline (MAAPster) is a comprehensive web tool designed by the CCR Collaborative Bioinformatics Resource (CCBR) that performs transcriptome analysis of human or mouse Affymetrix gene expression data. Samples may be uploaded locally or accessed from published data by entering a GEO Series identification number. MAAPster can analyze data from a single experiment that includes multiple samples, and the user may investigate multiple contrasts between groups of samples. Raw CEL files are analyzed using several Bioconductor packages in ‘R’, including limma and oligo. Output includes array probe quality control plots, sample quality control plots such as 3D PCA, differential gene expression analysis, gene expression heatmaps, pathway analysis and single sample GSEA analysis. 
                             </p>
-                        </div>
+                        
+                            <p>
+                                The pipeline supports the following commonly used Affymetrix chips. More may be added upon request.
+                            </p>
+                            <h3>Human:</h3>
+                            <ul>
+                                <li>Human Genome U133 Plus 2.0 Array</li>
+                                <li>GeneChip™ Human Genome U133A Array</li>
+                                <li>GeneChip™ Human Genome U133A 2.0 Array</li>
+                                <li>GeneChip™ Human Genome U133B Array</li>
+                                <li>GeneChip™ Human Gene 1.0 ST Array (v1 & v2)</li>
+                                <li>GeneChip™ Human Gene 1.1 ST Array Version 1</li>
+                                <li>GeneChip™ Human Gene 2.0 ST Array</li>
+                                <li>Clariom™ S Assay HT, human</li>
+                                <li>Clariom™ S Assay, human</li>
+                                <li>Human Genome U219 Array</li>
+                                <li>GeneChip Human Genome U95 Version 2</li>
+                                <li>GeneChip™ Human Transcriptome Array 2.0</li>
+                                <li>Human Exon 1.0 ST Array</li>
+                            </ul>
+
+                            <h3>Mouse:</h3>
+                            <ul>
+                                <li>GeneChip™ Mouse Gene 1.0 ST Array</li>
+                                <li>GeneChip™ Mouse Gene 1.1 ST Array</li>
+                                <li>GeneChip™ Mouse Gene 2.0 ST Array</li>
+                                <li>Clariom™ S Assay HT, mouse</li>
+                                <li>Clariom™ S Assay, mouse</li>
+                                <li>GeneChip™ Mouse Genome 430 2.0 Array</li>
+                                <li>GeneChip™ Mouse Genome 430A 2.0 Array</li>
+                                <li>GeneChip® Mouse Expression Set 430</li>
+                                <li>GeneChip® Murine Genome U74v2 Set</li>
+                                <li>Mouse Exon 1.0 ST Array</li>
+                            </ul>
+
+                         </div>
                     </section>
         </div>
         );
