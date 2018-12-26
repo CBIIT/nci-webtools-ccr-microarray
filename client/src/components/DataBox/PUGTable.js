926c359dbe89f969ef85930c36c61ba495bb01f1
jonkiky <jonkiky@gmail.com>
2018-12-26 12:11:55 -0500

    add link style to column pathway id
    Former-commit-id: 08203f2c327ed6316e25468a4d5beaa5e68646a4 [formerly ba9cbe3f56c97f82d19d8670257ed186f362b420 [formerly 747011909a784c7c8f26d056e97ca54ff2e4021c]]
    Former-commit-id: ca0824f8dedf35201ff4821cff34525b2309e663
    Former-commit-id: d10bc5bf22c6d23d91b83460294c81d93d6d5bca

--- client/src/components/DataBox/PUGTable.js
+++ client/src/components/DataBox/PUGTable.js
@@ -142,7 +142,7 @@ class PUGTable extends Component {
               render: (text, record, index) => {
 
                     return <div className="single-line" style={{"maxWidth":document.getElementsByClassName("ant-tabs-tabpane-active")[0].offsetWidth*0.1}}>
-                                    <span data-toggle="tooltip" data-placement="left" title={text}>{text}</span>
+                                    <span style={{"color":"#40a9ff"}} data-toggle="tooltip" data-placement="left" title={text}>{text}</span>
                                     </div>
 
                 }
