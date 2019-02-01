0b940b5a5f6a5309cfe438a147c71d6e1df6b44e
jonkiky <jonkiky@gmail.com>
2019-02-01 15:37:52 -0500

    change the size of RLE

--- client/src/components/Analysis/Analysis.js
+++ client/src/components/Analysis/Analysis.js
@@ -1349,7 +1349,7 @@ class Analysis extends Component {
 
 
                             let plot_layout = { showlegend: false }
-                            let plot_style = { "width": document.getElementsByClassName("ant-tabs-tabpane-active")[0].offsetWidth * 0.8, }
+                            let plot_style = { "width": document.getElementsByClassName("ant-tabs-tabpane-active")[0].offsetWidth * 0.9, }
 
                             let RLE = <Plot data={RLERenderData} layout={plot_layout}  style={plot_style}  useResizeHandler={true}/>
 
