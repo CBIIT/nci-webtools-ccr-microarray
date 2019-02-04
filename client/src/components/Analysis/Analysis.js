7a8f7d05621e6c5587af96da100e490c441d7d22
jonkiky <jonkiky@gmail.com>
2019-02-04 11:11:26 -0500

    change the size of heatmap

--- client/src/components/Analysis/Analysis.js
+++ client/src/components/Analysis/Analysis.js
@@ -1000,7 +1000,7 @@ class Analysis extends Component {
         document.getElementById("message-post-heatmap").innerHTML = "";
         let workflow = Object.assign({}, this.state.workflow);
         let link = "./images/" + workflow.projectID + "/heatmapAfterNorm.html"
-        let HeatMapIframe = <div><iframe title={"Heatmap"} src={link}  width={'100%'} height={'80%'} frameBorder={'0'}/></div>
+        let HeatMapIframe = <div><iframe title={"Heatmap"} src={link}  width={'100%'} height={'100%'} frameBorder={'0'}/></div>
         workflow.postplot.Heatmapolt = <div>{HeatMapIframe}</div>;
         this.setState({ workflow: workflow });
     }
