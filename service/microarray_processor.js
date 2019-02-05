d4d521c046a94ca84f6871f332519a58bebc32ff
jonkiky <jonkiky@gmail.com>
2019-02-05 11:14:23 -0500

    change email template

--- service/microarray_processor.js
+++ service/microarray_processor.js
@@ -103,7 +103,7 @@ function r(data, endCallback) {
         if (err) {
             logger.info("[Queue] Run Contrast fails ", err)
             logger.info("[Queue] sendMail to  ", data.email)
-            let subject = "MicroArray Contrast Results -" + dateFormat(now, "dd_mm_yyyy_h_MM") + "(FAILED)";
+            let subject = "MicroArray Contrast Results -" + dateFormat(now, "yyyy_mm_dd_h_MM") + "(FAILED)";
             let html = emailer.emailFailedTemplate(d[3], secondToDate(end / 1000), config.microarray_link + "?" + d[1], data.submit, d[1])
             emailer.sendMail(config.mail.from, data.email, subject, "text", html)
 
