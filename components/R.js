5c53241aca8a9456ca2146eb2bcf6c36e96fdeae
jonkiky <jonkiky@gmail.com>
2019-02-14 13:47:50 -0500

    update wrapper add try catch

--- components/R.js
+++ components/R.js
@@ -24,13 +24,13 @@ var execute = function(file, data, callback){
 
 	child.stdout.on('data', (d) => {
 		body += d.toString('utf8');
-		//logger.info("stdout:"+body);
+		logger.info("stdout:"+body);
 	});
 
 
 	child.stderr.on('data', (e) => {
 		err_message += e.toString('utf8');
-		//logger.info("stderr:"+err_message);
+		logger.info("stderr:"+err_message);
 	});
 
 	child.on('disconnect',()=>{
@@ -40,7 +40,7 @@ var execute = function(file, data, callback){
 
 	child.on('error',(Error)=>{
 		logger.info(Error.prototype.message)
-		//logger.info(Error.prototype.name)
+		logger.info(Error.prototype.name)
 
 	})
 
