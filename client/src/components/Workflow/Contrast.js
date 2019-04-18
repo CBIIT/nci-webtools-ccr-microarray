6764ca6a7c61fa7bb7f20b08744aef823896c016
jonkiky <jonkiky@gmail.com>
2019-04-18 15:03:43 -0400

    Remove redundant code

--- client/src/components/Workflow/Contrast.js
+++ client/src/components/Workflow/Contrast.js
@@ -34,11 +34,11 @@ class Contrast extends Component {
             let queueBlock = <div className="block ">
               <div  id="checkbox_queue">  <Checkbox checked={this.props.data.useQueue} onChange={this.handleSwitchChange} >Submit this job to a Queue</Checkbox></div>
                <div className="queueMessage" style={{"paddingLeft":"23px"}}>(Jobs currently enqueued: {this.props.data.numberOfTasksInQueue})</div>
-             <label className="title" > Email<span style={{color:"red","paddingLeft":"5px"}}> *</span></label>
-             <Input  disabled={!this.props.data.useQueue} id="email"  aria-label="input email"  id="input-email" placeholder="email"/>
-             <span className="queueMessage">Note: if sending to queue, when computation is completed, a notification will be sent to the e-mail entered above.</span>
-             <div><span className="err-message" id="message-use-queue"></span></div>
-              <div><span className="success-message" id="message-success-use-queue"></span></div>
+                 <label className="title" > Email<span style={{color:"red","paddingLeft":"5px"}}> *</span></label>
+                 <Input  disabled={!this.props.data.useQueue} id="email"  aria-label="input email"  id="input-email" placeholder="email"/>
+                 <span className="queueMessage">Note: if sending to queue, when computation is completed, a notification will be sent to the e-mail entered above.</span>
+                 <div><span className="err-message" id="message-use-queue"></span></div>
+                 <div><span className="success-message" id="message-success-use-queue"></span></div>
           </div>
 
 
@@ -70,66 +70,6 @@ class Contrast extends Component {
             <option value="RMA_Loess" >RMA plus Cyclic Loess</option>
           </select>);
 
-
-            // var content = "";
-            // content =
-            //     <div>
-            //    <div className="block">
-            //       <label className="title"  htmlFor="select-group-1">Choose Contrast To Show: <span style={{color:"red","paddingLeft":"5px"}}> *</span></label>
-            //       {group_1_content}
-            //       <label className="title"  htmlFor="select-group-2">VS: <span style={{color:"red","paddingLeft":"5px"}}> *</span></label>
-            //       {group_2_content}
-            //     </div>
-            //     <div className="block ">
-            //      <label className="title" htmlFor="select-group-1">Choose Normalization Method:</label>
-            //       {normalization_option}
-            //     </div>
-            //       {queueBlock}
-            //       {button}
-            //       <br/>
-            //    </div>
-
-            // if (options.length <= 1) {
-            //     // if the group have not be defined
-            //     content =
-            //         <div>
-            //         <div className="block ">
-            //         <label className="title" htmlFor="select-group-1">Choose Contrast To Show:</label>
-            //         <select id="select-group-1" className="ant-select-selection ant-select-selection--single" value={this.props.data.group_1} style={{ width: "100%" }}  disabled   aria-label="select Group 1">
-            //           <option value="-1">---select Group---</option>
-            //         </select>
-            //         <label className="title" htmlFor="select-group-2">VS:</label>
-            //          <select  id="select-group-2"  className="ant-select-selection ant-select-selection--single" value={this.props.data.group_2} style={{ width: "100%" }}  disabled aria-label="select Group 2">
-            //            <option value="-1">---select Group---</option>
-            //         </select>
-            //       </div>
-            //       <div className="block ">
-            //        <label className="title" htmlFor="select-group-1">Choose Normalization Method:</label>
-            //         {normalization_option}
-            //       </div>
-            //         {queueBlock}
-            //         {button}
-            //         <br/>
-            //       </div>
-            // } else {
-            //     content =
-            //         <div>
-            //        <div className="block">
-            //           <label className="title"  htmlFor="select-group-1">Choose Contrast To Show: <span style={{color:"red","paddingLeft":"5px"}}> *</span></label>
-            //           {group_1_content}
-            //           <label className="title"  htmlFor="select-group-2">VS: <span style={{color:"red","paddingLeft":"5px"}}> *</span></label>
-            //           {group_2_content}
-            //         </div>
-            //         <div className="block ">
-            //          <label className="title" htmlFor="select-group-1">Choose Normalization Method:</label>
-            //           {normalization_option}
-            //         </div>
-            //           {queueBlock}
-            //           {button}
-            //           <br/>
-            //        </div>
-
-            // }
             return (
                     <div>
                        <div className="block">
