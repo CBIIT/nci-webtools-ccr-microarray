238de37a641d31badf50b5498c193520a3047505
jonkiky <jonkiky@gmail.com>
2019-04-24 13:28:04 -0400

    fix PCA color code

--- client/src/components/Analysis/Analysis.js
+++ client/src/components/Analysis/Analysis.js
@@ -1177,8 +1177,10 @@ class Analysis extends Component {
             fetch('./api/analysis/getPCA', {
                     method: "POST",
                     body: JSON.stringify(params),
-                    processData: false,
-                    contentType: false
+                    credentials: "same-origin",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 })
                 .then(this.handleErrors)
                 .then(res => res.json())
@@ -1196,7 +1198,7 @@ class Analysis extends Component {
                                 if (pcaData.group_name[i].toLowerCase() == workflow2.group_2.toLowerCase() || pcaData.group_name[i].toLowerCase() == workflow2.group_1.toLowerCase()) {
                                     color = pcaData.color[i];
                                 } else {
-                                     if (color_for_others == "#000")
+                                    if (color_for_others == "#000")
                                         color_for_others = pcaData.color[i];
                                     color = color_for_others;
                                 }
@@ -1219,8 +1221,8 @@ class Analysis extends Component {
                                 }
                             });
                             for (let element in group_data) {
-                                let color ="";
-                                if (element.toLowerCase() == workflow2.group_2 || element.toLowerCase() == workflow2.group_1) {
+                                let color = "";
+                                if (element.toLowerCase() == workflow2.group_2.toLowerCase() || element.toLowerCase() == workflow2.group_1.toLowerCase()) {
                                     color = group_data[element]['color'];
                                 } else {
                                     color = color_for_others;
@@ -1323,8 +1325,10 @@ class Analysis extends Component {
             fetch('./api/analysis/getBoxplotAN', {
                     method: "POST",
                     body: JSON.stringify(params),
-                    processData: false,
-                    contentType: false
+                    credentials: "same-origin",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 })
                 .then(this.handleErrors)
                 .then(res => res.json())
@@ -1424,8 +1428,10 @@ class Analysis extends Component {
             fetch('./api/analysis/getNUSE', {
                     method: "POST",
                     body: JSON.stringify(params),
-                    processData: false,
-                    contentType: false
+                    credentials: "same-origin",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 }).then(res => res.json())
                 .then(result => {
                     document.getElementById("message-pre-nuse").innerHTML = "";
@@ -1458,14 +1464,16 @@ class Analysis extends Component {
             fetch('./api/analysis/getRLE', {
                     method: "POST",
                     body: JSON.stringify(params),
-                    processData: false,
-                    contentType: false
+                    credentials: "same-origin",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 }).then(this.handleErrors)
                 .then(res => res.json())
                 .then(result => {
                     if (result.status == 200) {
                         document.getElementById("message-pre-rle").innerHTML = "";
-                         let workflow = Object.assign({}, this.state.workflow);
+                        let workflow = Object.assign({}, this.state.workflow);
                         if (result.data != "") {
                             workflow.progressing = false;
                             let render_data = this.generateBOXPLOT(result, workflow);
@@ -1523,8 +1531,8 @@ class Analysis extends Component {
             if (v == workflow.group_2 || v == workflow.group_1) {
                 cMarker = BoxplotsData.color[i];
             } else {
-                if(color_for_others=="#000"){
-                     color_for_others = BoxplotsData.color[i]
+                if (color_for_others == "#000") {
+                    color_for_others = BoxplotsData.color[i]
                 }
                 cMarker = color_for_others;
             }
@@ -1591,8 +1599,10 @@ class Analysis extends Component {
             fetch('./api/analysis/getBoxplotBN', {
                     method: "POST",
                     body: JSON.stringify(params),
-                    processData: false,
-                    contentType: false
+                    credentials: "same-origin",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 }).then(this.handleErrors)
                 .then(res => res.json())
                 .then(result => {
