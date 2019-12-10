a670de84d73c959559ca82be4d8797faf0af2e24
Phyllip Cho <phyllip3@users.noreply.github.com>
2019-12-10 16:29:32 -0500

    fix heatmap path

--- client/src/components/Analysis/Analysis.js
+++ client/src/components/Analysis/Analysis.js
@@ -1247,7 +1247,7 @@ class Analysis extends Component {
         document.getElementById("message-post-heatmap").innerHTML = "";
         let workflow = Object.assign({}, this.state.workflow);
 
-        let link = "./images/" + workflow.projectID + "/" + workflow.heatmapolt_url
+        let link = "./images/" + workflow.projectID + workflow.heatmapolt_url
         let HeatMapIframe = <CIframe title={"Heatmap"} link={link} data={this.state.workflow} onLoadComplete={this.onLoadComplete} showLoading={this.showLoading} />;
         workflow.postplot.Heatmapolt = <div>{HeatMapIframe}</div>;
         this.setState({ workflow: workflow });
