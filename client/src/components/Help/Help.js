42be6ebabc7bafccbbdb1d4c45bc99f8b42db969
Phyllip Cho <phyllip3@users.noreply.github.com>
2020-04-08 17:45:53 -0400

    update help page and videos

--- client/src/components/Help/Help.js
+++ client/src/components/Help/Help.js
@@ -22,38 +22,37 @@ export default function Help() {
         </div>
         <h3>Input</h3>
         <p>There are 2 input options:</p>
-        <ul>
+        <ol>
           <li>
             <p>
-              1) Local: Analyze expression data by uploading CEL files from a local computer. Data
-              must be expression data in CEL file format, and all data must be from the same type of
+              Local: Analyze expression data by uploading CEL files from a local computer. Data must
+              be expression data in CEL file format, and all data must be from the same type of
               Affymetrix chip.
             </p>
-            <p>
-              <video loop controls>
-                <source src="./assets/img/loading-upload-file.webm" type="video/webm" />
-                <source src="./assets/img/loading-upload-file.mp4" type="video/mp4" />
-                Sorry, your browser doesn't support embedded videos.
-              </video>
-            </p>
+            <video preload="metadata" controls>
+              <source src="./assets/img/upload.mp4" type="video/mp4" />
+              Sorry, your browser doesn't support embedded videos.
+            </video>
           </li>
           <li>
             <p>
-              2) GEO: Analyze expression data from the Gene Expression Omnibus (GEO)
+              GEO: Analyze expression data from the Gene Expression Omnibus (GEO)
               https://www.ncbi.nlm.nih.gov/geo/ by providing the relevant series ID from the
-              experiment, as noted below. Enter the entire ID, starting with GSE. The experiment
-              must include data in CEL file format from the same type of Affymetrix chip.
+              experiment, as noted below. Enter the entire ID, starting with GSE. Samples from
+              different chips cannot be analyzed together. If the series ID contains samples from
+              multiple chips, choose the type of chip from the drop-down menu. Samples analyzed on
+              each chip will populate as the chip is chosen:
             </p>
+            <video preload="metadata" controls>
+              <source src="./assets/img/accession.mp4" type="video/mp4" />
+              <p>Sorry, your browser doesn't support embedded videos.</p>
+            </video>
             <p>
-              <video loop controls>
-                <source src="./assets/img/loading-with-accession-data.webm" type="video/webm" />
-                <source src="./assets/img/loading-with-accession-data.mp4" type="video/mp4" />{' '}
-                Sorry, your browser doesn't support embedded videos.
-              </video>
+              The following chips are supported, and additional Affymetrix mouse or human chips may
+              be added upon request. Currently, microRNA and single-cell analyses are not supported.
             </p>
-            <p>Currently, microRNA and single-cell analyses are not supported.</p>
           </li>
-        </ul>
+        </ol>
 
         <h3>Human</h3>
         <ul>
@@ -92,28 +91,39 @@ export default function Help() {
         <h3>Analysis</h3>
         <p>
           Once the files are uploaded, choose groups for each sample. At least 2 groups must be
-          created.
+          created. The control group should be entered last. For example:
         </p>
-        <p>Next, choose contrasts to compare,for example: </p>
-        <p>The control group should be entered last.</p>
-        <p>For example:</p>
+
         <p>
           <span>
             Disease
             <br />
-            VS
+            <b>VS</b>
             <br />
             Control
             <br />
           </span>
         </p>
+
         <p>
-          <video loop controls>
-            <source src="./assets/img/group.webm" type="video/webm" />
-            <source src="./assets/img/group.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
+          OPTIONAL: If batch effects are present in the dataset, assign batch names as shown. Batch
+          effects will be corrected for differential gene expression analysis by incorporating
+          batches into the linear model. For plots based on normalized gene expression and before
+          ssGSEA analysis, normalized gene expression is corrected for batch effects with the ComBat
+          function from the sva package (Leek, Johnson, Parker, Jaffe, & Storey, 2012).
         </p>
+
+        <h4>Manually Add Groups and Batches:</h4>
+        <video preload="metadata" controls>
+          <source src="./assets/img/manualGroup.mp4" type="video/mp4" />
+          <p>Sorry, your browser doesn't support embedded videos.</p>
+        </video>
+
+        <h4>Upload Groups and Batches:</h4>
+        <video preload="metadata" controls>
+          <source src="./assets/img/csvGroup.mp4" type="video/mp4" />
+          <p>Sorry, your browser doesn't support embedded videos.</p>
+        </video>
 
         <h4>Array Probe Quality Control:</h4>
         <p>
@@ -127,26 +137,22 @@ export default function Help() {
               each sample. These estimates are normalized to 1 across all samples, so a sample with
               a significantly higher value may be of lower quality (Carvalho & Irizarry, 2010).{' '}
             </p>
-            <p>
-              <video loop controls>
-                <source src="./assets/img/nuse.webm" type="video/webm" />
-                <source src="./assets/img/nuse.mp4" type="video/mp4" />
-                Sorry, your browser doesn't support embedded videos.
-              </video>
-            </p>
+
+            <video preload="metadata" controls>
+              <source src="./assets/img/nuse.mp4" type="video/mp4" />
+              Sorry, your browser doesn't support embedded videos.
+            </video>
           </li>
           <li>
             <p>
               The Relative Log Expression (RLE) plot compares the expression level of one probeset
               to the median expression of that probeset across all samples.{' '}
             </p>
-            <p>
-              <video loop controls>
-                <source src="./assets/img/RLE.webm" type="video/webm" />
-                <source src="./assets/img/RLE.mp4" type="video/mp4" />
-                Sorry, your browser doesn't support embedded videos.
-              </video>
-            </p>
+
+            <video preload="metadata" controls>
+              <source src="./assets/img/rle.mp4" type="video/mp4" />
+              Sorry, your browser doesn't support embedded videos.
+            </video>
           </li>
         </ul>
         <p>
@@ -162,52 +168,47 @@ export default function Help() {
           Each sample is normalized to correct for technical artifacts using the Robust Multichip
           Averaging (RMA) method described in (Irizarry et al., 2003). Optional cyclic loess is also
           available and can be performed if RMA is insufficient (Ballman, Grill, Oberg, & Therneau,
-          2004; Bolstad, Irizarry, Astrand, & Speed, 2003). All samples are used for normalization
-          regardless of group selections. Pre-normalization and post-normalization histograms, MA
-          plots and box plots display relative expression (represented by probe intensity) before
-          and after RMA. Data for the plots are generated with the oligo package (Carvalho &
+          2004; Bolstad, Irizarry, Astrand, & Speed, 2003). Only selected samples are used for
+          normalization. Pre-normalization and post-normalization histograms, MA plots and box plots
+          display relative expression (represented by probe intensity) before and after
+          normalization. Data for the plots are generated with the oligo package (Carvalho &
           Irizarry, 2010). Normalized data can be downloaded in the DEG-Enrichments Results tab.
         </p>
         <p>
           Histogram: The histogram displays the density of probes at each log-intensity. Each sample
-          is represented by a curved line. Before RMA, samples should be distributed randomly. An
-          outlier sample may be due to technical problems, and separate groupings of samples may
-          indicate batch effects. Either of these potential issues should be investigated further
-          before downstream analyses. After RMA, all samples lines should follow the same curve.
-          Again, if samples are clustered into groups, batch effects may be present.
+          is represented by a curved line. Before normalization, samples should be distributed
+          randomly. An outlier sample may be due to technical problems, and separate groupings of
+          samples may indicate batch effects. Either of these potential issues should be
+          investigated further before downstream analyses. After normalization, all samples lines
+          should follow the same curve. Again, if samples are clustered into groups, batch effects
+          may be present.
         </p>
-        <p>
-          <video loop controls>
-            <source src="./assets/img/histogram.webm" type="video/webm" />
-            <source src="./assets/img/histogram.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+
+        <video preload="metadata" controls>
+          <source src="./assets/img/histogram.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
 
         <p>
           MA plot: Log-ratios (M) are plotted against the average log-intensities (A). Plots are
           generated that compare each sample to the median of all samples. After RMA, the red sample
           line should be relatively equal to the blue median line.
         </p>
-        <p>
-          <video loop controls>
-            <source src="./assets/img/MAplot.webm" type="video/webm" />
-            <source src="./assets/img/MAplot.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+
+        <video preload="metadata" controls>
+          <source src="./assets/img/maplot.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
 
         <p>
           Boxplot: Displays the distribution of log-intensities for each sample. After RMA,
           distributions across all samples should be approximately the same.
         </p>
-        <p>
-          <video loop controls>
-            <source src="./assets/img/boxplot.webm" type="video/webm" />
-            <source src="./assets/img/boxplot.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+
+        <video preload="metadata" controls>
+          <source src="./assets/img/boxplot.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
 
         <p>
           Details regarding plots described above can be found in the oligo manual (Carvalho &
@@ -224,72 +225,64 @@ export default function Help() {
           together, and groups of samples should cluster separately from other groups.{' '}
         </p>
         <h4>3D PCA:</h4>
-        <p>
-          <video loop controls>
-            <source src="./assets/img/PCA.webm" type="video/webm" />
-            <source src="./assets/img/PCA.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+
+        <video preload="metadata" controls>
+          <source src="./assets/img/pca.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+
         <h4>Sample Similarity Heatmap:</h4>
 
-        <p>
-          <video loop controls>
-            <source src="./assets/img/heatmap.webm" type="video/webm" />
-            <source src="./assets/img/heatmap.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+        <video preload="metadata" controls>
+          <source src="./assets/img/heatmap.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
 
         <h3>Differential Gene Expression</h3>
         <p>
-          After normalization, determine differential gene expression between groups. MAAPster runs
-          analysis in the background with the limma package. Linear modeling is performed using
-          limma’s lmFit function, and differential gene expression is determined using the
-          contrasts.fit and eBayes functions (Ritchie et al, 2015). Using the toptable function,
-          false discovery rates are calculated to adjust p-values for multiple testing (Ritchie et
-          al., 2015). Detailed information regarding differential expression analysis can be found
-          in the limma manual
+          The DEG-Enrichments Results tab displays differentially expressed genes between groups.
+          MAAPster runs analysis in the background with the limma package. Linear modeling is
+          performed using limma’s lmFit function, and differential gene expression is determined
+          using the contrasts.fit and eBayes functions (Ritchie et al, 2015). Using the toptable
+          function, false discovery rates are calculated to adjust p-values for multiple testing
+          (Ritchie et al., 2015). Detailed information regarding differential expression analysis
+          can be found in the limma manual
           <a href="https://www.bioconductor.org/packages/release/bioc/vignettes/limma/inst/doc/usersguide.pdf">
             {' '}
             https://www.bioconductor.org/packages/release/bioc/vignettes/limma/inst/doc/usersguide.pdf.
           </a>
         </p>
-        <p>
-          <video loop controls>
-            <source src="./assets/img/deg.webm" type="video/webm" />
-            <source src="./assets/img/deg.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+
+        <video preload="metadata" controls>
+          <source src="./assets/img/deg.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
 
         <h3>Pathway Analysis</h3>
         <p>
           The top 500 significantly upregulated and top 500 significantly downregulated genes are
           extracted for pathway analysis (significance is determined as unadjusted p-value &lt;
-          0.05), Pathway analysis is performed with CCBR’s l2p software
+          0.05), Pathway analysis is performed with CCBR’s l2p R package{' '}
+          <a herf="https://github.com/CCBR/l2p">(https://github.com/CCBR/l2p).</a>
         </p>
         <h4>Gene Heatmaps:</h4>
         <p>
           Click on a pathway to generate a heatmap for the genes in the pathway. The heatmap will
-          include groups selected in the contrast, and will display scaled normalized gene
-          expression for samples in the groups.{' '}
+          include groups selected in the contrast and will display scaled normalized gene expression
+          for samples in the groups.{' '}
         </p>
 
-        <p>
-          <video loop controls>
-            <source src="./assets/img/pathway.webm" type="video/webm" />
-            <source src="./assets/img/pathway.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+        <video preload="metadata" controls>
+          <source src="./assets/img/pathway.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
 
         <h3>Single-sample GSEA (ssGSEA)</h3>
         <p>
-          ssGSEA is performed using the gsva package as described in (Hanzelmann et al., 2013).
-          Pathway enrichment scores are then analyzed to determine fold changes and p-values between
-          groups of samples, similar to the differential gene expression analysis described above.
-          Differential pathway enrichment is ranked by p-value, and the top 50 pathways are
+          ssGSEA is performed using the gsva package as previously described (Hanzelmann et al.,
+          2013). Pathway enrichment scores are then analyzed to determine fold changes and p-values
+          between groups of samples, similar to the differential gene expression analysis described
+          above. Differential pathway enrichment is ranked by p-value, and the top 50 pathways are
           displayed in a heatmap. Human gene set modules were downloaded from the BROAD Institute’s
           MSigDB
           <a href="http://software.broadinstitute.org/gsea/msigdb/collections.jsp">
@@ -301,13 +294,11 @@ export default function Help() {
           </a>
           .
         </p>
-        <p>
-          <video loop controls>
-            <source src="./assets/img/ssGSEA.webm" type="video/webm" />
-            <source src="./assets/img/ssGSEA.mp4" type="video/mp4" />
-            Sorry, your browser doesn't support embedded videos.
-          </video>
-        </p>
+
+        <video preload="metadata" controls>
+          <source src="./assets/img/ssgsea.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support embedded videos.
+        </video>
 
         <h3>Download Results</h3>
         <p>All tables and plots generated may be downloaded.</p>
@@ -337,6 +328,11 @@ export default function Help() {
             Scherf, U., & Speed, T. P. (2003). Exploration, normalization, and summaries of high
             density oligonucleotide array probe level data. Biostatistics, 4(2), 249-264.
             doi:10.1093/biostatistics/4.2.249
+          </li>
+          <li>
+            Leek, J. T., Johnson, W. E., Parker, H. S., Jaffe, A. E., & Storey, J. D. (2012). The
+            sva package for removing batch effects and other unwanted variation in high-throughput
+            experiments. Bioinformatics, 28(6), 882-883. doi:10.1093/bioinformatics/bts034
           </li>
           <li>
             Ritchie, M. E., Phipson, B., Wu, D., Hu, Y., Law, C. W., Shi, W., & Smyth, G. K. (2015).
